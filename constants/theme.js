9e005c0e2f47a4921a7fd46c66617caedea7bd90
Whisperer <whisperer.meta@gmail.com>
2025-12-23 17:52:32 +0200

    Add chef.png and theme colors

---
+++ constants/theme.js
@@ -0,0 +1,9 @@
+export const colors =  {
+    primary: '#8E64A1',
+    background: '#222222',
+    foreground: '#252525',
+    surface: '#2C2C2C',
+    border: '#424242',
+    muted: '#BEBDBE',
+    text: '#FAFAFA',
+}
